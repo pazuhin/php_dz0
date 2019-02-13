9f925fa13306b2e9ab224d7925f009b4fd955db0
Andrey Smirnov <andrew_1024@mail.ru>
2019-02-13 14:58:47 +0300

    dz 0

---
+++ dz_0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Description of what this module (or file) is doing.
+ */
+const TEST_CONSTANTA = 'asd';
+$userName = 'Igor';
+
+function showSomething()
+{
+    return 'SOMETHING';
+}
+if (1==1) {
+    echo 'hi';
+}
